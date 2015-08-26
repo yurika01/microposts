4427bbe021cf027ee0839b02db746e621e74c256
yurika01 <yurikapo01@yahoo.co.jp>
2015-08-26 22:09:43 +0000

    create message model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/new'
+
   root to: 'static_pages#home'
-  get 'signup' , to: 'users#new'
+  get 'signup', to: 'users#new'
+  get     'login'  => 'sessions#new'
+  post    'login'  => 'sessions#create'
+  delete  'logout' => 'sessions#destroy'
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823214948) do
+ActiveRecord::Schema.define(version: 20150826220908) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "name"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
